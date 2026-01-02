2aed9484f188a6c83747da9371acbf2c708fb68a
vacset <33067673+vacset@users.noreply.github.com>
2026-01-02 16:49:33 +0700

    Update library versions
    Update library versions and release as v2.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "me.seta.vacset.qrwari"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
